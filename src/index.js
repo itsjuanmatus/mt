ad2a246168ae58f68e3f75f97a5d10e8c17016d5
itsjuanmatus <juanmatusg16@gmail.com>
2021-09-25 18:36:00 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import './env'
+
+import * as userService from './services/users'
+import { connectAllDb } from './connectionManager'
+import * as connectionResolver from './middlewares/connectionResolver'
+
+
+const PORT = 8080
+
+const app = express()
+
+app.set('port', PORT)
+app.use(express.json())
+
+connectAllDb()
+
+app.use(connectionResolver.resolve)
+
+// API ROUTE
+app.get('/', (req, res, next) => {
+  res.json({ body: 'Hello multi-tenant application' })
+})
+
+app.get('/users', async (req, res, next) => {
+  const users = await userService.getAll()
+  res.json({ body: users })
+})
+
+app.listen(PORT, () => {
+  console.log(`Express server started at port: ${PORT}`)
+})
